be2679ef43ee66c62247b03f6a809644ae4f57d9
janelle-nomad <janelle-nomad@users.noreply.github.com>
2017-10-24 19:12:33 -0400

    Configured gulp archtecture for readability, and configured browsersync for realtime reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,5 @@
-var gulp = require('gulp'), 
-watch = require('gulp-watch'),  //Assign watch variable to gulp-watch function
-postcss = require('gulp-postcss'), 
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
+//require 'html and css styling' gulp file
+require('./app/gulp/tasks/styles');
 
-//Create a default task
-gulp.task('styles', function() {
-   return gulp.src('./app/assets/styles/styles.css') //gulp.src is an asyncronist function
-    .pipe(postcss([autoprefixer]))
-    .pipe(gulp.dest('./app/temp/styles'))
-    });
-    
-gulp.task('watch', function() {
-    watch('./app/assets/styles/**/*.css', function() {
-        gulp.start('styles');
-    });
-});
+//require gulp-watch file ~ to watch for updates
+require('./app/gulp/tasks/watch'); 
